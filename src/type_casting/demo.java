f964af70d96e5f8161241d779bfc927da3dc0907
Swaraj Jedhe <swarajjedhe26@gmail.com>
2024-01-09 21:46:44 +0530

    java_oops

---
+++ src/type_casting/demo.java
@@ -0,0 +1,26 @@
+package type_casting;
+
+public class demo {
+
+	public static void main(String[] args) {
+		
+		double d=11.11;
+		System.out.println(d);
+		int x =(int)d;
+		System.out.println(x);
+		System.out.println("__________________");
+		
+		int y =84;
+		System.out.println(y);
+		
+		char j=(char)y;
+		System.out.println(j);
+		System.out.println("____________________");
+		
+		float v =11.34f;
+		System.out.println(v);
+		int b =(int)d;
+		System.out.println(b);
+		
+	}
+}
---
+++ src/type_casting/pattern.java
@@ -0,0 +1,23 @@
+
+package type_casting;
+
+public class pattern {
+
+	public static void main(String[] args) {
+		
+		int d=11;
+		System.out.println("d "+d);
+		
+		double k=d;
+		System.out.println("k "+k);
+		
+		System.out.println("___________________-");
+		
+		char c='a';
+		
+		System.out.println("c "+c);
+		
+		int g=c;
+		System.out.println("g "+g);
+	}
+}
